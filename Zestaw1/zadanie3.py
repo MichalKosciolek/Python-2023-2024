aca0b3741b510f5c8684f3fbd48c4512153d961c
Mintter02 <michal.kosc20@gmail.com>
2023-10-20 19:38:28 +0200

    zadanie3 hours and minutes fix

--- Zestaw1/zadanie3.py
+++ Zestaw1/zadanie3.py
@@ -3,8 +3,14 @@ import time
 
 while True:
     now = dt.datetime.now()
+    hours = now.hour
+    if hours < 10:
+        hours = '0' + str(hours)
+    minutes = now.minute
+    if minutes < 10:
+        minutes = '0' + str(minutes)
     seconds = now.second
     if seconds < 10:
         seconds = '0' + str(seconds)
-    print(chr(16), ' ' * 4, now.hour, ':', now.minute, ':', seconds, ' ' * 4, chr(17), end='\r', sep='')
+    print(chr(16), ' ' * 3, hours, ':', minutes, ':', seconds, ' ' * 3, chr(17), end='\r', sep='')
     time.sleep(0.5)
