1102c527f7509a7d2ae84fbb328db7ab7d71ab81
MichalKosciolek <michal.kosc20@gmail.com>
2023-11-07 15:11:48 +0100

    zadanie2.3

---
+++ Zestaw2/zadanie3.py
@@ -0,0 +1,15 @@
+roman = input()
+
+dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 
+        'C': 100, 'D': 500, 'M': 1000}
+
+roman = roman.upper()
+    
+result = 0
+for i in range(len(roman)):
+    if i > 0 and dict[roman[i]] > dict[roman[i - 1]]:
+        result += dict[roman[i]] - 2 * dict[roman[i - 1]]
+    else:
+        result += dict[roman[i]]
+
+print(result)
