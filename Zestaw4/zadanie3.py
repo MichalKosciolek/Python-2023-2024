4485eea6cdda55d9ac159f91f592b816e23887b6
MichalKosciolek <michal.kosc20@gmail.com>
2023-12-06 18:11:06 +0100

    zadanie4.3

--- Zestaw4/zadanie3.py
+++ Zestaw4/zadanie3.py
@@ -3,7 +3,16 @@
 # tak, żeby kod poniżej drukował treści jak w komentarzach
 
 class A(object):
-    pass
+    def foo(self, x):
+        print(f'wykonanie foo({self}, {x})')
+
+    @classmethod
+    def class_foo(cls, x):
+        print(f'class_foo({cls}, {x})')
+
+    @staticmethod
+    def static_foo(x):
+        print(f'wykonanie static_foo({x})')
 
 a = A()
 a.foo(123) # wykonanie foo(<__main__.A object at 0x0000023A680D1F10>, 123)
@@ -13,11 +22,50 @@ A.class_foo(123) # ditto
 a.static_foo(123) # wykonanie static_foo(123)
 A.static_foo(123) # ditto
 
+print("-----------------")
+
 # podpunkt B)
 # zdefiniować dowolną klasę bazową dziedzicząca z ABC i posiadającą metodę abstrakcyjną
 # po czym zdefiniować ze dwie klasy potomne z odpowiednimi definicjami, zademonstrować w działaniu
 from abc import ABC, abstractmethod
 
+class Base(ABC):
+    @abstractmethod
+    def abstract_method(self):
+        pass
+
+class BaseChild1(Base):
+    def abstract_method(self):
+        print("Hello from BaseChild1")
+
+class BaseChild2(Base):
+    def abstract_method(self):
+        print("Hello from BaseChild2")
+
+c1 = BaseChild1()
+c1.abstract_method()
+
+c2 = BaseChild2()
+c2.abstract_method()
+
+print("-----------------")
+
 # podpunkt C)
 # zdefiniować dowolną klasę oraz atrybut klasy tak, że stanie się on atrybutem czytanym poprzez 
 # dekorator @property, a ustawianym za pomocą @nazwa.setter, pokazać w działaniu
+
+class MyClass:
+    def __init__(self):
+        self._nazwa = None
+
+    @property
+    def nazwa(self):
+        return self._nazwa
+
+    @nazwa.setter
+    def nazwa(self, value):
+        self._nazwa = value
+
+a = MyClass()
+a.nazwa = "Nazwa"
+print(a.nazwa)
